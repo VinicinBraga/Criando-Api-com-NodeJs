23d37d005e48bbaa68b7aa56fc139e5898ffa5bd
VinicinBraga <vinicius@bragamatta.com.br>
2021-09-14 16:52:19 -0300

    Integrando aplicaçao com o banco de dados Mongo

--- src/app/Controllers/UserController.js
+++ src/app/Controllers/UserController.js
@@ -1,12 +1,50 @@
+const User = require("../Models/User");
+const bcrypt = require("bcryptjs");
+const yup = require("yup");
+
 class UserController {
-  index(req, res) {
-    console.log(req.body);
-  }
   show(req, res) {
     const users = ["João", "Pedro"];
     return res.status(200).json({
       error: false,
       users,
+    });
+  }
+  async store(req, res) {
+    //Validação com o yup
+    let schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+      password: yup.string().required(),
+    });
+    if (!(await schema.isType(req.body))) {
+      return res.status(400).json({
+        error: true,
+        message: "Error to insert user on MongoDB",
+      });
+    }
+
+    //desestruturação dos dados da req
+    const { name, email, password } = req.body;
+
+    //criarndo a constante data com os dados
+    const data = { name, email, password };
+
+    //criptografia com o bcrypty
+    data.password = await bcrypt.hash(data.password, 8); //cria um criptografia de 8 caracteres
+
+    //inserção no banco de dados
+    await User.create(data, (err) => {
+      if (err) {
+        return res.status(400).json({
+          error: true,
+          message: "Error to insert user on MongoDB",
+        });
+      }
+      return res.status(200).json({
+        error: false,
+        message: "successfully registered user",
+      });
     });
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const UserController = require("./app/Controllers/UserController");
 
 const routes = new Router();
 
-routes.post("/user", UserController.index);
+routes.post("/user", UserController.store);
 routes.get("/user", UserController.show);
 
 module.exports = routes;
